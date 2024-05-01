4f9a88977d2c1fb3172e72a19eaf02847278ccd7
Alex Walls <alexanderwalls92@gmail.com>
2024-05-01 17:00:34 +0100

    written a few test outlines, installed hamcrest

---
+++ EmployeesDatabase/src/test/java/com/sparta/gwoc/dao/EmployeesDAOTest.java
@@ -0,0 +1,56 @@
+package com.sparta.gwoc.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeesDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("test to see if getAllEmployees method returns an Array of Employees")
+    void testToSeeIfGetAllEmployeesMethodReturnsAnArrayOfEmployees() {
+        
+        org.junit.jupiter.api.Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("test to see if an empty database returns an empty Array")
+    void testToSeeIfAnEmptyDatabaseReturnsAnEmptyArray() {
+        
+        org.junit.jupiter.api.Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("given a list of Employee Data, the database should be populated with that data")
+    void givenAListOfEmployeeDataTheDatabaseShouldBePopulatedWithThatData() {
+        
+        org.junit.jupiter.api.Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("given an empty list of Employee data, no data is added to the database")
+    void givenAnEmptyListOfEmployeeDataNoDataIsAddedToTheDatabase() {
+
+        org.junit.jupiter.api.Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("given an empty list of Employee Data, a warning message should be logged")
+    void givenAnEmptyListOfEmployeeDataAWarningMessageShouldBeLogged() {
+
+        org.junit.jupiter.api.Assertions.fail("Not implemented");
+    }
+
+
+}
