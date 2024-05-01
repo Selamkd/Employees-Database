d0c9ed14ef8a9ce15409a523a756f908b9a7c883
“selam” <Selamkd@users.noreply.github.com>
2024-05-01 16:32:23 +0100

    logger ready to use

---
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/utils/TestLogger.java
@@ -0,0 +1,23 @@
+package com.sparta.gwoc.utils;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class TestLogger {
+    public static void main(String[] args) {
+     final Logger LOGGER = Logger.getLogger(TestLogger.class.getName());
+        try {
+            LoggerUtil.setup(LOGGER);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        LOGGER.severe("This is a severe message.");
+        LOGGER.warning("This is a warning message.");
+        LOGGER.info("This is an info message.");
+        LOGGER.config("This is a config message.");
+        LOGGER.fine("This is a fine message.");
+        LOGGER.finer("This is a finer message.");
+        LOGGER.finest("This is a finest message.");
+    }
+}
