ead51fef230a389f7a0ebc72c9f675efaf258aef
Howard <howardchung04@yahoo.com.hk>
2024-05-01 15:43:06 +0100

    Update Employee.java

--- EmployeesDatabase/src/main/java/com/sparta/gwoc/Employee.java
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/Employee.java
@@ -1,7 +1,6 @@
 package com.sparta.gwoc;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public record Employee(int empID, String prefix, String firstName, char middleInitial, String lastName,
                        char gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoin, int salary) {
