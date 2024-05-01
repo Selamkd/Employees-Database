82d3a3b5c5b7e3105082f9899dadcd9cd63966b4
Howard <howardchung04@yahoo.com.hk>
2024-05-01 10:03:02 +0100

    Update Validator.java
    Added validator for issue 9 and 10

--- EmployeesDatabase/src/main/java/com/sparta/gwoc/Validator.java
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/Validator.java
@@ -1,5 +1,7 @@
 package com.sparta.gwoc;
 
+import java.time.LocalDate;
+
 public class Validator {
 
     private boolean validEmployeeID(int empID){
@@ -10,15 +12,29 @@ public class Validator {
         return gender == 'M' || gender == 'F';
     }
 
-    private boolean validCharacter(String string){
-        return string != null && string.matches("[A-Z]");
+    private boolean validCharacter(char character){
+        return character >='a' && character <= 'z';
     }
 
-    private static boolean validEmail(String email){
+    private boolean validEmail(String email){
         return email != null
                 && email.matches("^[A-Za-z]+(\\.[A-Za-z]+)*" + // Matches local part
                 "@" + // Matches @
                 "[A-Za-z]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$" // Matches Domain
         );
     }
+
+    private static boolean validString(String string){
+        return string != null && string.matches("^[A-Za-z]+");
+    }
+
+    private static boolean validPrefix(String prefix){
+        return prefix != null && prefix.matches("^[A-Za-z]+[.]");
+    }
+
+    private static boolean validSalary(int salary){
+        return salary > 0;
+    }
+
+
 }
