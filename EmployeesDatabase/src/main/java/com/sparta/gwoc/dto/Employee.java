3ff04326b435e16c347b8aaa8352a2e448e1e083
Howard <howardchung04@yahoo.com.hk>
2024-05-02 13:23:02 +0100

    Update Employee.java
    Changed empID to String instead of int

--- EmployeesDatabase/src/main/java/com/sparta/gwoc/dto/Employee.java
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/dto/Employee.java
@@ -2,7 +2,7 @@ package com.sparta.gwoc.dto;
 
 import java.time.LocalDate;
 
-public record Employee(int empID, String prefix, String firstName, char middleInitial, String lastName,
+public record Employee(String empID, String prefix, String firstName, char middleInitial, String lastName,
                        char gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoin, int salary) {
 
     @Override
