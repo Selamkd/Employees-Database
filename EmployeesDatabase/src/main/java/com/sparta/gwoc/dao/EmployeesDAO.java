aaff8ce7b9c8637d20ed0b6faa569c2865ff011d
Alex Walls <alexanderwalls92@gmail.com>
2024-05-02 14:30:12 +0100

    Added DAO interface and some refactoring

--- EmployeesDatabase/src/main/java/com/sparta/gwoc/dao/EmployeesDAO.java
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/dao/EmployeesDAO.java
@@ -6,49 +6,110 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeesDAO {
+public class EmployeesDAO implements DAOInterface {
     private static Connection dbConnection;
     private static PreparedStatements preparedStatements;
     private static ResultSet resultSet;
 
 
-
-    public void init() {
+    @Override
+    public void openDBConnection() {
         DatabaseProperties properties = DatabaseProperties.loadPropertiesFromFile();
         ConnectionManager connectionManager = new ConnectionManager(properties);
         dbConnection = connectionManager.getConnection();
-
     }
 
-    public List<Employee> getAllEmployees() throws SQLException {
+    @Override
+    public List<Employee> getAllEmployeeRecords() {
         List<Employee> employeeList = new ArrayList<>();
-        Statement statement = dbConnection.createStatement();
-        ResultSet resultSet = statement.executeQuery(PreparedStatements.GET_ALL_EMPLOYEES);
-        System.out.println(resultSet);
-        while (resultSet.next()) {
-            Employee employee = new Employee(
-                    resultSet.getString(1),
-                    resultSet.getString(2),
-                    resultSet.getString(3),
-                    resultSet.getString(4).charAt(0),
-                    resultSet.getString(5),
-                    resultSet.getString(6).charAt(0),
-                    resultSet.getString(7),
-                    resultSet.getDate(8).toLocalDate(),
-                    resultSet.getDate(9).toLocalDate(),
-                    resultSet.getInt(10)
 
-            );
-            employeeList.add(employee);
+        try (Statement statement = dbConnection.createStatement();
+             ResultSet resultSet = statement.executeQuery(PreparedStatements.GET_ALL_EMPLOYEES);) {
+            System.out.println(resultSet);
+            while (resultSet.next()) {
+                Employee employee = new Employee(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4).charAt(0),
+                        resultSet.getString(5),
+                        resultSet.getString(6).charAt(0),
+                        resultSet.getString(7),
+                        resultSet.getDate(8).toLocalDate(),
+                        resultSet.getDate(9).toLocalDate(),
+                        resultSet.getInt(10)
+
+                );
+
+                employeeList.add(employee);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return employeeList;
     }
 
+    @Override
+    public Employee getEmployeeRecordByID(String id) {
+        return null;
+    }
+
+    @Override
+    public int deleteEmployeeRecordByID(String id) {
+
+        try (Statement statement = dbConnection.createStatement();
+             ResultSet resultSet = statement.executeQuery(PreparedStatements.DELETE_EMPLOYEE_BY_ID);) {
+            System.out.println(resultSet);
+            while (resultSet.next()) {
+                Employee employee = new Employee(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4).charAt(0),
+                        resultSet.getString(5),
+                        resultSet.getString(6).charAt(0),
+                        resultSet.getString(7),
+                        resultSet.getDate(8).toLocalDate(),
+                        resultSet.getDate(9).toLocalDate(),
+                        resultSet.getInt(10)
+
+                );
+
+                //employeeList.add(employee);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public int insertEmployees(List<Employee> employeeList) {
+        return 0;
+    }
+
+    @Override
+    public int updateFirstNameById(String id, String newFirstName) {
+        return 0;
+    }
+
+
+    public void closeDBConnection() {
+        try {
+            dbConnection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args) throws SQLException {
         EmployeesDAO dao = new EmployeesDAO();
-        dao.init();
-        List<Employee> employees = dao.getAllEmployees();
-        dbConnection.close();
+        dao.openDBConnection();
+        List<Employee> employees = dao.getAllEmployeeRecords();
+        dao.closeDBConnection();
         System.out.println(employees);
     }
 
--- EmployeesDatabase/src/main/java/com/sparta/gwoc/dao/PreparedStatements.java
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/dao/PreparedStatements.java
@@ -2,5 +2,6 @@ package com.sparta.gwoc.dao;
 
 public interface PreparedStatements {
     String GET_ALL_EMPLOYEES = "SELECT * FROM employees";
+    String DELETE_EMPLOYEE_BY_ID = "DELETE FROM employees WHERE empID = ?";
 
 }
