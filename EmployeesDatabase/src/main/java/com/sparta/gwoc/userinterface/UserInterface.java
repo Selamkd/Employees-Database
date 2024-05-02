a91cd74222ca2af1ed67fe34f55e5bcb9062e3d3
“selam” <Selamkd@users.noreply.github.com>
2024-05-02 16:22:18 +0100

    userInterface added

---
+++ EmployeesDatabase/src/main/java/com/sparta/gwoc/userinterface/UserInterface.java
@@ -0,0 +1,69 @@
+package com.sparta.gwoc.userinterface;
+
+import com.sparta.gwoc.dao.DAOInterface;
+import com.sparta.gwoc.dao.EmployeesDAO;
+import com.sparta.gwoc.dto.Employee;
+
+import java.util.List;
+
+public class UserInterface implements DAOInterface {
+
+   private EmployeesDAO employeesDAO;
+
+
+    public UserInterface(EmployeesDAO employeesDAO) {
+        this.employeesDAO = employeesDAO;
+    }
+
+    public void openDBConnection(){
+        employeesDAO.openDBConnection();
+    }
+
+    public void closeDBConnection(){
+        employeesDAO.closeDBConnection();
+    }
+
+    public List<Employee> getAllEmployeeRecords(){
+       return employeesDAO.getAllEmployeeRecords();
+    }
+
+    public Employee getEmployeeRecordByID(String id) {
+        return employeesDAO.getEmployeeRecordByID(id);
+    }
+
+   public int deleteEmployeeRecordByID(String id){
+        int rowsUpdated =  employeesDAO.deleteEmployeeRecordByID(id);
+        if(rowsUpdated > 0){
+            System.out.println("Employee with ID " + id + "deleted successfully.");
+        }else{
+            System.out.println("No employee found with ID" + id);
+        }
+        return rowsUpdated;
+   }
+
+
+   public int insertEmployees(List <Employee> employeeList){
+        int rowsUpdated = employeesDAO.insertEmployees(employeeList);
+        if(rowsUpdated > 0){
+            System.out.println("Employees inserted successfully.");
+        }else{
+            System.out.println("Failed to insert employees.");
+        }
+
+        return rowsUpdated;
+   }
+
+   public int updateFirstNameById(String id, String newFirstName){
+        int rowsAffected =  employeesDAO.updateFirstNameById(id,newFirstName);
+
+        if(rowsAffected > 0){
+            System.out.println("Employee with ID " + id + "'s first name updated to " + newFirstName + ".");
+        }else{
+            System.out.println("No employee found with ID" + id);
+        }
+
+        return rowsAffected;
+   }
+
+}
+
